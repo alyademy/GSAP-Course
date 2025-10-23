91da3730ba4bbe919c0f86be44faf8dc8991651a
Oleksandra Dementych <alyad96@gmail.com>
2025-10-23 19:09:00 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let firstName = 'Oleksandra'
+let age = 29
+let city = 'Toronto'
+let country = 'Canada'
+let hobbies = 'reading, sports, traveling'
+
+console.log(`Hello, my name is ${firstName}. I am ${age} years old. I live in ${city}, ${country}. My hobbies are ${hobbies}. And now I'm studying at Pixel Perfect School.`);
